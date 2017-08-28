888eca28d454cf92a7b358c94cfc08a2931fe96d
lakepics <lkepics@kepics.com>
2017-08-28 16:24:37 -0700

    make captions work on all media types

--- template-gallery.php
+++ template-gallery.php
@@ -72,11 +72,12 @@
                 }
                 echo '<a class="fancybox-media" data-fancybox-type="iframe" href="' . $picture['video'] . '">';
                 echo '<img class="" src="'. $picture_src[0] .'" alt="'. $imgTitle .'" />';
+                    echo '<div class="caption-photos">' . $picture['caption_photo'] . '</div>';
                 echo '</a>';
             }
             else {
-                echo '<div class="test" style="position: absolute; bottom: 0px; left: 0px; display: inline-block; background: rgba(66, 66, 66, 0.44); font-family: "Proxima Nova Italic", sans-serif; font-style: italic; padding: 5px 10px; color: white;">' . $picture['caption'] . '</div>';
                 echo '<img class="" src="'. $picture_src[0] .'" alt="'. $imgTitle .'" />';
+                echo '<div class="caption-photos">' . $picture['caption_photo'] . '</div>';
             }
             echo '</div>';
         }
