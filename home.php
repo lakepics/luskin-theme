d20a388d9b719abfef2d8b5dff9d9544ad769870
Gagandeep Singh <singh.enso@gmail.com>
2018-03-20 12:28:08 -0700

    update with home.php

--- home.php
+++ home.php
@@ -2,22 +2,21 @@
 
 <div id="main-content" role="main">
 
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <?php showHeroSlides(); ?>
     <div class="blog-ribbon">
         <div class="container clearfix"><span class="blog-issue"><?php custom_breadcrumbs(); ?></span></div>
     </div>
-    <div class="container clearfix">
-        <h1>Blog</h1>
-    </div>
     <div class="container  clearfix">
         <div class="blog-content page-col-twothird test">
-    <div <?php post_class( 'post-listing' ) ?> id="post-
-        <?php the_ID(); ?>">
-                <?php query_posts('posts_per_page=12'); /*1, 2*/
+
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <?php showHeroSlides(); ?>
+
+            <?php query_posts('posts_per_page=6'); /*1, 2*/
             if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-                <?php if ( has_post_thumbnail() ) : ?>
-                <article class="blog-post">
+            <?php if ( has_post_thumbnail() ) : ?>
+            <article class="blog-post">
+                <div <?php post_class( 'post-listing' ) ?> id="post-
+                    <?php the_ID(); ?>">
                     <div class="blog-post-single">
                         <a class="post-img" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
                             <?php the_post_thumbnail('full'); ?>
@@ -39,31 +38,26 @@
                             <p><a class="button read-more-btn" href="<?php the_permalink(); ?>">Read More</a></p>
                         </div>
                     </div>
-                </article>
-        </div>
-                <!-- blog-post -->
-                <?php endif; ?>
-                <?php endwhile; ?>
+                </div>
+            </article>
+            <!-- blog-post -->
+            <?php endif; ?>
 
-                <div class="nav-previous alignleft">
-                    <?php next_posts_link( 'Older posts' ); ?>
-                </div>
-                <div class="nav-next alignright">
-                    <?php previous_posts_link( 'Newer posts' ); ?>
-                </div>
-                <?php wp_reset_query(); ?>
-                <!-- Add the pagination functions here. -->
-            </div>
-            <!-- Sidebar right -->
-            <div class="page-col-onethird news-sidebar-content">
-                <?php if ( is_active_sidebar( 'blog-sidebar' ) ) : ?>
-                <div id="blog-sidebar" class="" role="complementary">
-                    <?php dynamic_sidebar( 'blog-sidebar' ); ?>
-                </div>
-                <?php endif; ?>
-            </div>
-            <!-- sidebar end -->
+            <?php endwhile; ?>
+
+            <?php wp_reset_query(); ?>
+
+
         </div>
+        <!-- Sidebar right -->
+        <div class="page-col-onethird news-sidebar-content">
+            <?php if ( is_active_sidebar( 'blog-sidebar' ) ) : ?>
+            <div id="blog-sidebar" class="" role="complementary">
+                <?php dynamic_sidebar( 'blog-sidebar' ); ?>
+            </div>
+            <?php endif; ?>
+        </div>
+        <!-- sidebar end -->
     </div>
 
     <?php endwhile; ?>
